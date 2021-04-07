802f5bc983eefce34c839541aa9e44b81921f7fa
mochsyahroni <msyahroni891@gmail.com>
2021-04-07 16:37:03 +0700

    Web vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="app">
-    <HelloWorld msg="TOKO ELEKTRONIK"/>
+    <Navbar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/navbar.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,4 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Menu</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Kategory</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Kritik&Saran</a></li>
-    </ul>
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-  background: yellow;
-  height: 25px;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
